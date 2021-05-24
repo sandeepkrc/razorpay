44963e2a04fe163303d12021cabf10207b380b4c
sandeepkrc <raj.sandip96@gmail.com>
2021-05-24 19:14:11 +0530

    optimize code

--- payapp/views.py
+++ payapp/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
 import razorpay
 from .models import Coffe
-import smtplib
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.core.mail import send_mail,EmailMessage
 
 def home(request):
     if request.method =="POST":
@@ -17,20 +13,14 @@ def home(request):
         payment = client.order.create({'amount':amount,'currency':'INR','payment_capture':'1'})
         coffe = Coffe(name=name,amount=amount,email=email,payment_id = payment['id'],)
         coffe.save()
-        print("hello")
-        # print(payment)
-        print(name)
-        print(amount)
         return render(request,'index.html',{'payment':payment})
     return render(request,"index.html")
 
 
 @csrf_exempt
 def success(request):
-
     if request.method =='POST':
         a = request.POST
-
         order_id = ""
         for key , val in a.items():
             if key == 'razorpay_order_id':
@@ -39,45 +29,4 @@ def success(request):
         user = Coffe.objects.filter(payment_id=order_id).first()
         user.paid = True
         user.save()
-            
-            # if user is not None:
-            #     user.Paid =True#paid
-            #     user.save()
-
-
-
-
-
-            
-            
-            # send_mail(
-            #        'Razor pay ',
-            #        'Your payment is successful. i use this format in email https://docs.djangoproject.com/en/3.2/topics/email/',
-            #        settings.EMAIL_HOST_USER,
-            #        ['user.email'],
-            #        fail_silently=False,
-            #        )
-            
-            
-            
-            
-            # msg_plain = render_to_string('email.txt')
-            # msg_html = render_to_string('email.html')
-            # send_mail("your payment is received 2",
-            # msg_plain , 
-            # settings.EMAIL_HOST_USER,
-            # str(['user.email']) , html_message = msg_html)#email shuld be str
-            
-            # msg_plain = render_to_string('email.txt')
-            # msg_html = render_to_string('email.html')
-            # send_mail(
-            # msg_html,
-            # msg_plain,
-         
-            # 'raj.sandip96@gmail.com',
-            # ['user.email'],fail_silently=False,)
-
-
-        
-                
     return render(request,"success.html")
